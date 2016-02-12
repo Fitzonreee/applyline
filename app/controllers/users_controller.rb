21ca28e5e55df8f5bd8841adb23a5326b5c4f2c2
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-02-11 16:23:19 -0800

    Built and styled registration and login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+  def index
+    # show registration / login page
+    @home_page = true
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'jobs#index'
+  # root 'jobs#index'
   # root 'users#index'
+
+  get 'welcome' => 'users#index'
+  get 'pipeline' => 'jobs#index'
+
 
   get 'jobs/show'
 
